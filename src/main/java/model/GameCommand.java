0b0f9245f7acd473896d0263867bdacd36c8b5cc
khj1 <dhepd8849@gmail.com>
2022-12-10 20:39:20 +0900

    feat: 재시작 여부 명령어 유효성 검증 추가

---
+++ src/main/java/model/GameCommand.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.Arrays;
+
+public enum GameCommand {
+    RESTART("R"),
+    QUIT("Q");
+
+    private static final String INVALID_GAME_COMMAND_INPUT = "R 또는 Q만 입력할 수 있습니다.";
+
+    private final String command;
+
+    GameCommand(String command) {
+        this.command = command;
+    }
+
+    public static GameCommand from(String command) {
+        return Arrays.stream(values())
+                .filter(gameCommand -> gameCommand.isSameCommand(command))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(INVALID_GAME_COMMAND_INPUT));
+    }
+
+    private boolean isSameCommand(String command) {
+        return this.command.equals(command);
+    }
+}
---
+++ src/test/java/model/CommandTest.java
@@ -0,0 +1,19 @@
+package model;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class CommandTest {
+
+    @ParameterizedTest()
+    @ValueSource(strings = {"", " ", "S"})
+    void 잘못된_명령어_입력_예외처리(String command) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> GameCommand.from(command));
+    }
+}
