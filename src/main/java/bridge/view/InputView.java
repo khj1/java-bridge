97c850210962add4c15e7f39a75476438283e98a
khj1 <dhepd8849@gmail.com>
2022-12-10 20:46:22 +0900

    feat: 재시작 여부를 입력 받는 기능 추가

--- src/main/java/bridge/view/InputView.java
+++ src/main/java/bridge/view/InputView.java
@@ -1,6 +1,7 @@
 package bridge.view;
 
 import bridge.model.BridgeSize;
+import bridge.model.GameCommand;
 import bridge.model.Moving;
 import bridge.utils.NumberUtils;
 import camp.nextstep.edu.missionutils.Console;
@@ -12,6 +13,7 @@ public class InputView {
 
     private static final String REQUEST_BRIDGE_SIZE = "다리의 길이를 입력해주세요.";
     private static final String REQUEST_MOVING = "이동할 칸을 선택해주세요. (위: U, 아래: D)";
+    private static final String REQUEST_GAME_COMMAND = "게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)";
 
     /**
      * 다리의 길이를 입력받는다.
@@ -35,7 +37,9 @@ public class InputView {
     /**
      * 사용자가 게임을 다시 시도할지 종료할지 여부를 입력받는다.
      */
-    public String readGameCommand() {
-        return null;
+    public GameCommand readGameCommand() {
+        System.out.println(REQUEST_GAME_COMMAND);
+
+        return GameCommand.from(Console.readLine());
     }
 }
